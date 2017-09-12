d3a271aa7eadc52b6478da22762ea562ea029d8c
cholorecto <pocholorecto123@gmail.com>
2017-09-12 16:01:52 +0800

    Remove unused imports

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.contrib.auth.validators import ASCIIUsernameValidator, UnicodeUsernameValidator
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
