b6d20ff058a8961e6bafafe4ec5ddf2d5faef810
cholorecto <pocholorecto123@gmail.com>
2017-09-13 17:32:52 +0800

    login function has no design yet

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,19 @@
 from django import forms
-#from django.contrib.auth import authenticate, login,
+from django.contrib.auth import authenticate
 
 class LoginForm(forms.Form):
     email = forms.EmailField(label="Email")
     password = forms.CharField(widget=forms.PasswordInput)
+    user = None
 
-    def auth(self, *args, **kwargs):
-        import pdb;pdb.set_trace()
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        auth = authenticate(email=email,password=password)
+        if not auth:
+            raise forms.ValidationError("Check your password!")
+        else:
+            self.user = auth
+
+        return cleaned_data
