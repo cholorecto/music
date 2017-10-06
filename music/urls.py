9f37eb6a92d7df0e25845bf49eb1f846c9216831
cholorecto <pocholorecto123@gmail.com>
2017-10-06 11:38:40 +0800

    ES is working but it needs to rebuild the indexes

--- music/urls.py
+++ music/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     url(r'^videoplayer/', include('videoplayer.urls')),
     url(r'^music/',include('playlist.urls')),
     url(r'^$', TemplateView.as_view(template_name='index.html')),
-#    url(r'^search/', include('haystack.urls')),
+    url(r'^search/', include('haystack.urls')),
 ]
